8b43eddeb8b437820e3f4e903e2c07b47f27357d
Martyn <zogacc@gmail.com>
2024-12-10 19:53:29 +0100

    remove code

--- src/shared/store/user.ts
+++ src/shared/store/user.ts
@@ -1,6 +1,3 @@
 import { makeVar } from '@apollo/client'
-import { MeFragment } from '__generated__/graphql'
-import { Maybe } from 'shared/types'
 
-export const userVar = makeVar<Maybe<MeFragment>>(null)
 export const isUserAccessDeniedVar = makeVar<boolean>(false)
--- src/shared/types/types.ts
+++ src/shared/types/types.ts
@@ -1,48 +1,3 @@
-import { SetupsPredictFragment } from '__generated__/graphql'
-
-export interface UserMetadata {
-  host: string
-  'accept-encoding': string
-  'x-forwarded-for': string
-  'cf-ray': string
-  'x-forwarded-proto': string
-  'cf-visitor': string
-  'sec-ch-ua': string
-  dnt: string
-  'sec-ch-ua-mobile': string
-  'user-agent': string
-  'sec-ch-ua-platform': string
-  accept: string
-  origin: string
-  'sec-fetch-site': string
-  'sec-fetch-mode': string
-  'sec-fetch-dest': string
-  referer: string
-  'accept-language': string
-  'if-none-match': string
-  priority: string
-  'cf-connecting-ip': string
-  'cdn-loop': string
-  'cf-ipcity': string
-  'cf-ipcontinent': string
-  'cf-ipcountry': string
-  'cf-iplatitude': string
-  'cf-iplongitude': string
-  'cf-region': string
-  'cf-region-code': string
-  'cf-timezone': string
-  'x-envoy-external-address': string
-  'x-request-id': string
-  'x-envoy-decorator-operation': string
-  'x-envoy-peer-metadata-id': string
-  'x-envoy-peer-metadata': string
-  'x-envoy-attempt-count': string
-  'apollo-require-preflight': string
-  'x-b3-traceid': string
-  'x-b3-spanid': string
-  'x-b3-sampled': string
-}
-
 export type FieldType = 'email' | 'username' | 'password' | 'number'
 
 export type AssetId =
@@ -83,7 +38,5 @@ export interface AssetPrice {
   price: number
   timestamp: number
 }
-
-export type WinnerModalParticipantsList = readonly SetupsPredictFragment[]
 
 export type BetDirection = 'UP' | 'DOWN'
